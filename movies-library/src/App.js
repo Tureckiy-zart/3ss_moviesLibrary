fa7f68873edeac58d57b443790a0b618681b860a
Yevhen  (Jay) Zhdanov <yevhen.zhdanov@3ss.tv>
2021-04-04 14:35:19 +0300

    initial commit

---
+++ movies-library/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Loader from "./Components/Loader/Loader";
+
+function App() {
+  return (
+    <>
+      <Loader />
+      <div className="App">lkjhkjxhcvcvx</div>
+    </>
+  );
+}
+
+export default App;
